2e2518c493bc89eb80ec3a17740379c86db6d213
GabrielDantasDs <gabri4k@gmail.com>
2024-04-16 14:13:29 -0300

    db setting up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
-import Link from "next/link";
+import { db } from "~/server/db";
+export const dynamic = "force-dynamic";
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const mockUrls = [ 
+    'https://utfs.io/f/9b7c49b4-c462-4643-b132-e29e53494ea2-d0250a.jpg',
+    'https://utfs.io/f/3e75955b-36fe-4df3-9fcc-439f81f23a64-jaws4v.jpg',
+    'https://utfs.io/f/2e5b70b7-35ea-4bc8-8933-a729b998fa24-9mc9xc.jpg',
+    'https://utfs.io/f/faa5bfcd-5210-40d8-9433-54082c3af4ed-8hic38.png'
+  ];
+
+  const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url
+  }));
+
+  const posts = await db.query.posts.findMany();
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        Hello, gallery in progress
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {posts.map(post => (
+          <span>{post.name}</span>
+        ))}
+        {mockImages.map(image => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
       </div>
     </main>
   );
