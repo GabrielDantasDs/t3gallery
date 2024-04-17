34db5e0a350af1cd814e12f92b5410d7928c5c53
GabrielDantasDs <gabri4k@gmail.com>
2024-04-16 23:57:48 -0300

    start implement parallel routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
 export const dynamic = "force-dynamic";
@@ -9,16 +10,18 @@ export default async function HomePage() {
 
   function Images() {
     return (
-      <div className="flex flex-wrap gap-4 justify-center">
+      <div className="flex flex-wrap justify-center gap-4">
         {images.map((image, index) => (
           <div key={image.id} className="flex h-48 w-48 flex-col">
-            <Image
-              src={image.url}
-              alt={image.name}
-              style={{ objectFit: "contain" }}
-              width={480}
-              height={480}
-            />
+            <Link href={`/img/${image.id}`}>
+              <Image
+                src={image.url}
+                alt={image.name}
+                style={{ objectFit: "contain" }}
+                width={480}
+                height={480}
+              />
+            </Link>
             <div>{image.name}</div>
           </div>
         ))}
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -11,6 +11,24 @@ export async function getMyImages() {
     where: (model, { eq }) => eq(model.userId, user.userId),
     orderBy: (model, { desc }) => desc(model.id),
   });
-
+  
   return images;
 }
+
+export async function getMyImage(id: number) {
+  const user = auth();
+
+  if (!user.userId) throw new Error("Unauthorized");
+
+  const image = await db.query.images.findFirst({
+    where: (model, { eq }) => eq(model.id, id),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+
+  if (!image) throw new Error("Image not found");
+  
+  if (image.userId !== user.userId) throw new Error("Unauthorized");
+  
+  return image;
+}
+
