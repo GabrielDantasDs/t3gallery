7d10e5d4a24efa573b93abf411f33e07d4dace2c
GabrielDantasDs <gabri4k@gmail.com>
2024-04-17 00:29:34 -0300

    move to the grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default async function HomePage() {
 
   function Images() {
     return (
-      <div className="flex flex-wrap justify-center gap-4">
-        {images.map((image, index) => (
+      <div className="flex flex-wrap justify-center gap-4 p-4">
+        {[...images, ...images, ...images, ...images, ...images, ...images, ...images, ...images].map((image, index) => (
           <div key={image.id} className="flex h-48 w-48 flex-col">
             <Link href={`/img/${image.id}`}>
               <Image
