48fa699059f0047da5a624122c4dcb51565d5dc4
GabrielDantasDs <gabri4k@gmail.com>
2024-04-16 23:28:27 -0300

    use next image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
 export const dynamic = "force-dynamic";
@@ -8,10 +9,16 @@ export default async function HomePage() {
 
   function Images() {
     return (
-      <div className="flex flex-wrap gap-4">
+      <div className="flex flex-wrap gap-4 justify-center">
         {images.map((image, index) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} />
+          <div key={image.id} className="flex h-48 w-48 flex-col">
+            <Image
+              src={image.url}
+              alt={image.name}
+              style={{ objectFit: "contain" }}
+              width={480}
+              height={480}
+            />
             <div>{image.name}</div>
           </div>
         ))}
@@ -21,7 +28,11 @@ export default async function HomePage() {
 
   return (
     <main className="">
-      <SignedOut><div className="h-full w-full text-2xl text-center">Please Sign In, to access gallery</div></SignedOut>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please Sign In, to access gallery
+        </div>
+      </SignedOut>
       <SignedIn>
         <Images />
       </SignedIn>
